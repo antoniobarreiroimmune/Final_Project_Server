094f8cee96109b69c3cd289c90d4a5753522496d
antoniobarreiroimmune <antonio.barreiro.dw@immune.institute>
2024-04-15 17:36:08 +0200

    Vuelve a local

--- db/index.js
+++ db/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 
 const MONGO_URI =
-  process.env.MONGODB_URI || "mongodb+srv://antoniobmrb:T191087q@cluster0.flixhs4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+  process.env.MONGODB_URI ||  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/Final_Project_Server";
 
 mongoose
   .connect(MONGO_URI)
